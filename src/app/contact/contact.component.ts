997ba1a1bcd3da5339dbaa3e8bab2c6ec5f80094
Mvcampbell3 <mvcampbell3@gmail.com>
2019-12-25 22:40:16 -0500

    added image loaded for contacts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  loaded: boolean = false;
+
+  images: string[] = [
+    '../../assets/images/icons/githubwhite.png',
+    '../../assets/images/icons/linkedin.svg'
+  ]
+
   constructor() { }
 
   ngOnInit() {
+    this.loadPictures()
+  }
+
+  loadPictures() {
+    let loaded = 0;
+    this.images.forEach(image => {
+      const newImage = new Image();
+      newImage.src = image;
+      newImage.onload = () => {
+        loaded ++;
+        if (loaded === this.images.length) {
+          this.loaded = true;
+        }
+      }
+    })
   }
 
 }
