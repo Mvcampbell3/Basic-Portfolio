b0f10382a5e0316e070f41f056dd10cc032c7d32
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-27 23:39:47 -0500

    changed out gifs for longer duration ones

--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -14,7 +14,7 @@ export class ProjectPageComponent implements OnInit {
     {
       name: 'Teammate Finder',
       img_still: 'still img',
-      img_gif: '../../assets/images/projects/teammateopt.gif',
+      img_gif: '../../assets/images/projects/teammate.gif',
       link_github: 'https://github.com/Mvcampbell3/find-a-tm',
       link_website: 'https://teammate-finder-app.herokuapp.com/',
       description: 'This is a MEAN stack application which allows for users to self-rate their skills when it comes to online games, and then find other players near that skill level on various consoles. The main purpose of this application was to teach myself Typescript and Angular',
@@ -24,7 +24,7 @@ export class ProjectPageComponent implements OnInit {
     {
       name: 'Rover-Reddit',
       img_still: 'still img',
-      img_gif: '../../assets/images/projects/rovermove.gif',
+      img_gif: '../../assets/images/projects/roverreddit.gif',
       link_github: 'https://github.com/DavidWeid/Space-Pair-Project',
       link_website: 'https://rover-reddit.herokuapp.com/',
       description: 'Rover Reddit was the final project in my coding bootcamp. We made a MERN stack application which interfaced rover pictures from Mars onto a social media platform. The images were from the NASA Mars Rovers OpenAPI, and we also scraped news articles from space.com',
@@ -34,7 +34,7 @@ export class ProjectPageComponent implements OnInit {
     {
       name: 'Better To-do',
       img_still: 'still img',
-      img_gif: '../../assets/images/projects/bettertodomove.gif',
+      img_gif: '../../assets/images/projects/bettertodo.gif',
       link_github: 'https://github.com/Mvcampbell3/betterToDo',
       link_website: 'https://better-todos.herokuapp.com/',
       description: 'This is a to-do application where you can group tasks by project, and also choose which projects to display. It is an Express-Handlebars application which uses MongoDB and NodeJS.',
@@ -44,7 +44,7 @@ export class ProjectPageComponent implements OnInit {
     {
       name: 'Roosevelt Way (WIP)',
       img_still: 'still img',
-      img_gif: '../../assets/images/projects/rooseveltopt.gif',
+      img_gif: '../../assets/images/projects/rooseveltway.gif',
       link_github: 'https://github.com/Mvcampbell3/parks-and-rec',
       link_website: 'https://roosevelt-way.herokuapp.com',
       description: 'This is a WIP MEAN stack application which will provide information to a user about the National Parks of the U.S. It will also give the user the ability to make travel itineraries, which will keep track of the parks they want to see on a given trip. The main purpose of this site is to give my parents a place where they can find all of the information they need.',
@@ -54,7 +54,7 @@ export class ProjectPageComponent implements OnInit {
     {
       name: 'Yahtzee React',
       img_still: 'still img',
-      img_gif: '../../assets/images/projects/yahtzeemove.gif',
+      img_gif: '../../assets/images/projects/yahtzee.gif',
       link_github: 'https://github.com/Mvcampbell3/YahtzeeReact',
       link_website: 'https://yahtzee-react.herokuapp.com/',
       description: 'This is a MERN stack application which allows users to play the dice game Yahtzee, and if they are logged in, track thier high scores. This project has gone through many evolutions. Because the scoring system is very complex, it was a very good way for me to learn several concepts in web development.',
@@ -64,7 +64,7 @@ export class ProjectPageComponent implements OnInit {
     {
       name: 'Trivia Madness',
       img_still: 'still img',
-      img_gif: '../../assets/images/projects/triviamove.gif',
+      img_gif: '../../assets/images/projects/trivia.gif',
       link_github: 'https://github.com/Mvcampbell3/triviaMadness',
       link_website: 'https://triviamadness.herokuapp.com/',
       description: 'MERN Stack application where users create and play trivia games. There is a lot of opportunity to improve the scope if this application through things such as a an overall scoring system or ability to leave comments on quizzes. I built this application to better understand form interaction in React and state management.',
@@ -74,7 +74,7 @@ export class ProjectPageComponent implements OnInit {
     {
       name: 'Rover Hooks',
       img_still: 'still img',
-      img_gif: '../../assets/images/projects/roverhooksopt.gif',
+      img_gif: '../../assets/images/projects/roverhooks.gif',
       link_github: 'https://github.com/Mvcampbell3/hook-space',
       link_website: 'https://rover-pics-react.netlify.com/rover',
       description: 'This is a React front-end website which displays Spirit and Oppotunity rover pictures from Mars using the NASA OpenAPI. The main purpose of this site was to experiment learning React-Hooks and parallax CSS',
